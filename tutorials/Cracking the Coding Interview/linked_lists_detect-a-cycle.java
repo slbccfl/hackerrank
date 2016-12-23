6a91061e78d4abe0304d97c0e11c901059abfdcc
Steven Barnes <slbccfl@gmail.com>
2016-12-23 14:35:23 -0500

    adding two code challeges

---
+++ tutorials/Cracking the Coding Interview/linked_lists_detect-a-cycle.java
@@ -0,0 +1,15 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    
+    
+
+}
