165f8a6f2bf58fb3d36a7fc058dacf2072960cde
Steven Barnes <slbccfl@gmail.com>
2017-08-05 19:33:21 -0400

    Completed challenge

--- bfs-shortest-reach-in-a-graph/Solution.java
+++ bfs-shortest-reach-in-a-graph/Solution.java
@@ -15,21 +15,20 @@ public class Solution {
 //	}
     public static class Graph {
         public static ArrayList<ArrayList<Integer>> adjacencyList; // list of adjacent nodes
-        public HashMap<Integer, Integer> nodes; // key: node#, value: distance from starting node.
+        public HashMap<Integer, Integer> distances; // key: node#, value: distance from starting node.
 
-	    private HashMap<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
-	    private Queue<Integer> queue = new LinkedList<Integer>();
+	    private HashMap<Integer, Boolean> settled = new HashMap<Integer, Boolean>();
 	    
         public Graph(int size) {
         	adjacencyList = new ArrayList<ArrayList<Integer>>(size);
-        	visited = new HashMap<Integer, Boolean>(size);
+        	settled = new HashMap<Integer, Boolean>(size);
         	ArrayList<Integer> emptyNodeArray;
-            nodes = new HashMap<Integer, Integer>(size);
+            distances = new HashMap<Integer, Integer>(size);
             for (int i = 0; i < size; i++) {
             	emptyNodeArray = new ArrayList<Integer>();
             	adjacencyList.add(i, emptyNodeArray);
-            	nodes.put(i, -1); // initialize all node distances to -1
-            	visited.put(i, false); // initialize all nodes to not visited
+            	distances.put(i, Integer.MAX_VALUE); // initialize all node distances largest possible Integer value
+            	settled.put(i, false); // initialize all nodes to not visited
             }
         }
 
@@ -47,43 +46,55 @@ public class Solution {
         }
         
         public void shortestReach(int startId) { // 0 indexed
-            queue.add(startId);
-            nodes.put(startId, 0);
+            distances.put(startId, 0);
             int currentNode, adjacentNode, distance, newDistance, oldDistance;
-                 
-            while (!queue.isEmpty()) {
-            	currentNode = queue.poll();
-            	distance = nodes.get(currentNode);
+            currentNode = startId;
+            
+                
+            while (currentNode != -1) {
+            	distance = distances.get(currentNode);
             	ArrayList<Integer> adjacentNodes = adjacencyList.get(currentNode);
-            	for (int i = 0; i < adjacentNodes.size(); i++) {
+            	int s = adjacentNodes.size();
+            	for (int i = 0; i < s; i++) {
             		adjacentNode = adjacentNodes.get(i);
-            		if (adjacentNode == startId) continue;
-	            	oldDistance = nodes.get(adjacentNode);
+            		if (adjacentNode == startId || settled.get(adjacentNode)) continue;
+	            	oldDistance = distances.get(adjacentNode);
 	            	newDistance = distance + 6;
-	            	if (oldDistance == -1 || newDistance < oldDistance) {
-	                	nodes.put(adjacentNode, newDistance);
+	            	if (newDistance < oldDistance) {
+	                	distances.put(adjacentNode, newDistance);
 	            	}
-	            	if (!visited.get(adjacentNode)) {
-	            		queue.add(adjacentNode);
-	            	}
+	            	
             	}
-            	visited.put(currentNode, true);
+            	settled.put(currentNode, true);
+            	// find minimum distance among unsettled nodes
+            	int minDistance = Integer.MAX_VALUE;
+            	
+            	for(Map.Entry<Integer, Integer> entry : distances.entrySet()) {
+            		if (settled.get(entry.getKey())) continue;
+            		distance = entry.getValue();
+            	    if (distance < minDistance) {
+            	    	minDistance = distance;
+            	    	currentNode = entry.getKey();
+            	    }
+            	}
+            	if (minDistance == Integer.MAX_VALUE) {
+            		currentNode = -1;
+            	}
             }
             
-//            ArrayList<Integer>  returnArrayList = new ArrayList<Integer>();
-
-            Iterator<Map.Entry<Integer, Integer>> it = nodes.entrySet().iterator();
+            Iterator<Map.Entry<Integer, Integer>> it = distances.entrySet().iterator();
             while (it.hasNext()) {
             	Map.Entry<Integer, Integer> nodeEntry = it.next();
             	currentNode = nodeEntry.getKey();
             	distance = nodeEntry.getValue();
-//            	returnArrayList.add(distance);
+            	if (distance == Integer.MAX_VALUE) distance = -1;
                 if (currentNode != startId) {
                     System.out.print(distance);
-                    System.out.print(" ");
+                    if (it.hasNext()) {
+                        System.out.print(" ");
+                    }
                 }
             }
-            System.out.println();   
             
             return;
         }
@@ -116,6 +127,8 @@ public class Solution {
             int startId = scanner.nextInt() - 1;
 //            int[] distances = 
             graph.shortestReach(startId);
+
+            if (t != queries - 1) System.out.println();   
  
 //            for (int i = 0; i < distances.length; i++) {
 //                if (i != startId) {
