507419c97bd36261c931505d6a97f474a29d0a34
Steven Barnes <slbccfl@gmail.com>
2017-08-27 20:11:24 -0400

    In-process: Dijkstra: Shortest Reach 2 -- Almost, almost done....

---
+++ dijkstra_shortest_reach_2/src/dijkstra_shortest_reach_2/Solution.java
@@ -0,0 +1,135 @@
+package dijkstra_shortest_reach_2;
+
+import java.io.*;
+import java.util.*;
+
+class Edge {
+	long weight;
+	int startNode;
+	int endNode;
+	
+	public Edge(int sN, int eN, long w) {
+		startNode = sN;
+		endNode = eN;
+		weight = w;
+	}
+}
+
+class Node {
+	int nodeID;
+	long distance;
+	LinkedList<Edge> adjacencyList; 
+	
+	public Node(int r, Edge edge) {
+		nodeID = r;
+		distance = Integer.MAX_VALUE;
+		adjacencyList = new LinkedList<Edge>();
+		if (edge != null) adjacencyList.add(edge);
+	}
+}
+public class Solution {
+
+    static HashMap<Integer, Node> graph = new HashMap<Integer, Node>();
+	static HashMap<Integer, Boolean>settled;
+//	static HashMap<Integer, Long>distances;
+	static LinkedList<Edge> SPTgraph;
+	static int numberOfNodes, numberOfEdges;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        numberOfNodes = in.nextInt();
+        numberOfEdges = in.nextInt();
+//        Node nodex, nodey;
+//        LinkedList<Edge> edges;
+        settled = new HashMap<Integer, Boolean>();
+//        for (int i = 0; i < numberOfNodes; i++) {
+//            visited.put(i + 1, false);
+//        }
+        for(int i = 0; i < numberOfEdges; i++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            settled.put(x, false);
+            settled.put(y, false);
+//            distances.put(x, Long.MAX_VALUE);
+//            distances.put(y, Long.MAX_VALUE);
+            long z = in.nextInt();
+            Edge edge = new Edge(x,y,z);
+            addToGraph(x, edge);
+            addToGraph(y, edge);
+        }
+        int startNodeID = in.nextInt();
+//        distances.put(startNodeID, 0L);
+        dijkstraSPT(startNodeID);
+        Iterator<Map.Entry<Integer, Node>> it = graph.entrySet().iterator();
+        while (it.hasNext()) {
+        	Map.Entry<Integer, Node> graphEntry = (Map.Entry)it.next();
+        	Node node = graphEntry.getValue();
+        	System.out.println(node.distance);
+        }
+
+        in.close();
+    }
+
+	static void addToGraph(int x, Edge edge) {
+		Node nodex;
+		if (graph.containsKey(x)) {
+		    nodex = graph.get(x);
+			nodex.adjacencyList.add(edge);
+		} else {
+			nodex = new Node(x, edge);
+		}
+		graph.put(x, nodex);
+	}
+	
+    static void dijkstraSPT(int startNodeID) {
+    	// using Dijkstra's algorithm to assemble a Shortest Path Tree for all nodes ...
+
+    	SPTgraph = new LinkedList<Edge>();
+    	List<Edge> edgesQueue = new ArrayList<Edge>();
+    	Node node = graph.get(startNodeID);
+    	node.distance = 0;
+    	edgesQueue = queueNodeEdges(edgesQueue, node);
+    	Edge lowestEdge;
+    	
+    	while (settled.containsValue(false)) { 
+        	lowestEdge = findLowestEdge(edgesQueue);
+        	Node newNode = graph.get(lowestEdge.endNode);
+        	edgesQueue = queueNodeEdges(edgesQueue, newNode);
+    	}
+    	return;
+    }
+    static List<Edge> queueNodeEdges(List<Edge> startingEdgesQueue, Node node) {
+    	settled.put(node.nodeID, true);
+    	List<Edge> edgesQueue = new ArrayList<Edge>();
+    	for (Edge edge : startingEdgesQueue) {
+    		if (!settled.get(edge.endNode)) edgesQueue.add(edge);
+    	}
+    	LinkedList<Edge> edges = node.adjacencyList;
+    	for (Edge edge : edges) {
+    		Integer adjacentNodeID = null;
+    		if (edge.startNode == node.nodeID) adjacentNodeID = edge.endNode;
+    		if (edge.endNode == node.nodeID) adjacentNodeID = edge.startNode;
+    		if (!settled.get(adjacentNodeID)) {
+        		edge.endNode = adjacentNodeID;
+        		edge.startNode = node.nodeID;
+    			edgesQueue.add(edge);
+    		}
+    	}
+    	return edgesQueue;
+    }
+    static Edge findLowestEdge(List<Edge> edgesQueue) {
+    	long lowestDistance = Long.MAX_VALUE;
+    	Edge lowestEdge = null;
+    	for (Edge edge : edgesQueue) { 
+    		long nodeDistance = graph.get(edge.startNode).distance + edge.weight;
+    		if (nodeDistance < lowestDistance && !settled.get(edge.endNode)) {
+    			lowestEdge = edge;
+    			lowestDistance = nodeDistance;
+    		}
+    	}
+    	SPTgraph.add(lowestEdge);
+    	graph.get(lowestEdge.endNode).distance = lowestDistance;
+    	return lowestEdge;
+    }
+}
---
+++ dijkstra_shortest_reach_2/test/dijkstra_shortest_reach_2/TestSolution.java
@@ -0,0 +1,117 @@
+package dijkstra_shortest_reach_2;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+public class TestSolution {
+
+	@Test
+	public void testAddToGraph() {
+		Solution.graph.clear();
+        int x = 1, y = 2;
+        long z = 0;
+        Edge edge = new Edge(x,y,z);
+        Solution.addToGraph(x, edge);
+        Solution.addToGraph(y, edge);
+		assertEquals(2, Solution.graph.size());
+		Node node = Solution.graph.get(x);
+		assertEquals(1, node.nodeID);
+		assertSame(edge, node.adjacencyList.peek());
+		node = Solution.graph.get(y);
+		assertEquals(2, node.nodeID);
+		assertSame(edge, node.adjacencyList.peek());
+	}
+	
+	@Test
+	public void testQueueNodeEdges() {
+		Solution.graph.clear();
+    	List<Edge> edgesQueue = new ArrayList<Edge>();
+        Solution.settled = new HashMap<Integer, Boolean>();
+    	Integer[][] testEdges = {
+    			{0,1,4},
+    			{0,7,8},
+    			{2,1,8},
+    			{1,7,11},
+    			{7,8,7},
+    			{6,7,1},
+    			{6,8,6},
+    			{6,5,2},
+    			{2,8,2},
+    			{2,3,7},
+    			{2,5,4},
+    			{3,5,14},
+    			{3,4,9},
+    			{4,5,10}
+    	};
+    	for (int i = 0; i < testEdges.length;i++) {
+    		int x = testEdges[i][0];
+    		int y = testEdges[i][1];
+            Solution.settled.put(x, false);
+            Solution.settled.put(y, false);
+    		Long z = Long.valueOf(testEdges[i][2].longValue());
+            Solution.addToGraph(x, new Edge(x,y,z));
+            Solution.addToGraph(y, new Edge(x,y,z));
+    	}
+    	Edge lowestEdge;
+    	edgesQueue = Solution.queueNodeEdges(edgesQueue, Solution.graph.get(7));
+    	lowestEdge = Solution.findLowestEdge(edgesQueue);
+    	assertEquals(1, Solution.graph.get(lowestEdge.endNode).distance);
+    	edgesQueue = Solution.queueNodeEdges(edgesQueue, Solution.graph.get(6));
+    	lowestEdge = Solution.findLowestEdge(edgesQueue);
+    	assertEquals(2, Solution.graph.get(lowestEdge.endNode).distance);
+    	edgesQueue = Solution.queueNodeEdges(edgesQueue, Solution.graph.get(5));
+    	lowestEdge = Solution.findLowestEdge(edgesQueue);
+    	assertEquals(4, Solution.graph.get(lowestEdge.endNode).distance);
+    	edgesQueue = Solution.queueNodeEdges(edgesQueue, Solution.graph.get(2));
+    	lowestEdge = Solution.findLowestEdge(edgesQueue);
+    	assertEquals(2, Solution.graph.get(lowestEdge.endNode).distance);
+    	edgesQueue = Solution.queueNodeEdges(edgesQueue, Solution.graph.get(8));
+    	lowestEdge = Solution.findLowestEdge(edgesQueue);
+    	assertEquals(6, Solution.graph.get(lowestEdge.endNode).distance);
+    	
+	}
+	
+	@Test
+	public void testDijkstraSPT() {
+		Solution.graph.clear();
+    	TreeMap<Long, LinkedList<Edge>> edgesQueue = new TreeMap<Long, LinkedList<Edge>>();
+        Solution.settled = new HashMap<Integer, Boolean>();
+    	Integer[][] testEdges = {
+    			{0,1,4},
+    			{0,7,8},
+    			{2,1,8},
+    			{1,7,11},
+    			{7,8,7},
+    			{6,7,1},
+    			{6,8,6},
+    			{6,5,2},
+    			{2,8,2},
+    			{2,3,7},
+    			{2,5,4},
+    			{3,5,14},
+    			{3,4,9},
+    			{4,5,10}
+    	};
+    	for (int i = 0; i < testEdges.length;i++) {
+    		int x = testEdges[i][0];
+    		int y = testEdges[i][1];
+            Solution.settled.put(x, false);
+            Solution.settled.put(y, false);
+    		Long z = Long.valueOf(testEdges[i][2].longValue());
+            Solution.addToGraph(x, new Edge(x,y,z));
+            Solution.addToGraph(y, new Edge(x,y,z));
+    	}
+    	Solution.dijkstraSPT(0);
+        long SPTdistance = 0;
+        for (Edge edge : Solution.SPTgraph) {
+        	System.out.println(edge.startNode + " - " + edge.endNode + ": " + Solution.graph.get(edge.endNode).distance);
+        	SPTdistance += edge.weight;
+        }
+
+        assertEquals(42, SPTdistance);
+	}
+
+}
