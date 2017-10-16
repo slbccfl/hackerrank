1d011efc882f56933d077c9dd1d2fe7fc6083269
Steven Barnes <slbccfl@gmail.com>
2017-10-16 18:57:34 -0400

    Still not good enough. Still timing out for test cases #4 and beyond. Now changing to a true compacted prefix tree, using points to the input string instead of replicating substrings in the processing.

--- how_many_substrings/src/how_many_substrings/Solution.java
+++ how_many_substrings/src/how_many_substrings/Solution.java
@@ -10,11 +10,15 @@ import java.util.regex.*;
 class TrieNode {
 	boolean isEnd;
 	TrieNode parentNode;
-	String string;
+//	String string;
+	int stringStart;
+	int stringEnd;
 	TrieNode[] letterArray;
 	
-	public TrieNode() {
-		this.string = "";
+	public TrieNode(int start, int end) {
+		this.stringStart = start;
+		this.stringEnd = end;
+//		this.string = "";
 		this.letterArray = new TrieNode[26];
 	}
 }
@@ -23,6 +27,7 @@ public class Solution {
 
 	static TrieNode trieTreeRoot;
 	static long startTime = System.currentTimeMillis();
+	static String s = null;
 
     public static void main(String[] args) {
     	
@@ -37,7 +42,7 @@ public class Solution {
 		}
     	int n = Integer.parseInt(line.split("\\s")[0]);
     	int q = Integer.parseInt(line.split("\\s")[1]);
-    	String s = null;
+//    	String s = null;
 		try {
 			s = br.readLine();
 		} catch (IOException e) {
@@ -49,27 +54,28 @@ public class Solution {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-	    	int left = Integer.parseInt(line.split("\\s")[0]);
-	    	int right = Integer.parseInt(line.split("\\s")[1]);
+	    	int subSStart = Integer.parseInt(line.split("\\s")[0]);
+	    	int subSEnd = Integer.parseInt(line.split("\\s")[1]);
+	    	int subSLength = subSStart - subSEnd + 1;
             
-            trieTreeRoot = new TrieNode();
-            String subS = s.substring(left,right+1);
+            trieTreeRoot = new TrieNode(subSStart,subSEnd);
+//            String subS = s.substring(left,right+1);
             
 //        	long queryStartTime = System.currentTimeMillis();
 //	    	timeStamp("Test case " + a0 + " -- Left:  " + left + " Right: " + right + "   subS.length(): " + subS.length());
 //	    	System.out.println(subS);
 
-	    	for (int ssLength = subS.length();ssLength >= 0; ssLength--) {
-	    		int beginIndex = subS.length() - ssLength;
-	    		insertPrefix(trieTreeRoot, subS.substring(beginIndex));
-            }
+//	    	for (int ssLength = subS.length();ssLength >= 0; ssLength--) {
+//	    		int beginIndex = subS.length() - ssLength;
+//	    		insertPrefix(trieTreeRoot, subS.substring(beginIndex));
+//            }
 	        
 //			long endTime = System.currentTimeMillis( );
 //			float queryElapsedTime = endTime - queryStartTime;
 //			System.out.println("Query Elapsed Milliseconds: " + queryElapsedTime + " -- Characters/millisecond: " + (subS.length() / queryElapsedTime));
 //        	long ouputStartTime = System.currentTimeMillis();
         	
-          System.out.println(countNodesInTrie(trieTreeRoot));	        
+//          System.out.println(countNodesInTrie(trieTreeRoot));	        
           
 //			endTime = System.currentTimeMillis( );
 //			System.out.println("Output Elapsed Milliseconds: " + (endTime - ouputStartTime));
@@ -84,76 +90,76 @@ public class Solution {
 //		System.out.println(title);
 //	}
     
-    static void insertPrefix(TrieNode currentNode, String subS) {
-    	// Check of subS is a prefix of the current node's prefix, is so no action needed, return
-    	if (subS.length() < currentNode.string.length() && subS.equals(currentNode.string.substring(0, subS.length()))) return;
-    	// Check of current node's prefix is a prefix of subS, 
-    	if (subS.length() > currentNode.string.length() && currentNode.string.equals(subS.substring(0, currentNode.string.length()))) {
-    		char nextChar = subS.charAt(currentNode.string.length());
-    		int index = nextChar - 'a';
-    		String subSSuffix = subS.substring(currentNode.string.length(), subS.length());
-    		if (currentNode.letterArray[index] != null) {
-    			insertPrefix(currentNode.letterArray[index], subSSuffix);
-    		} else {
-    			boolean letterArrayIsEmpty = true;
-    			for (int i = 0; i < currentNode.letterArray.length; i++) {
-    				if (currentNode.letterArray[i] != null) {
-    					letterArrayIsEmpty = false;
-    					break;
-    				}
-    			}
-    			if (letterArrayIsEmpty) {
-    				currentNode.string = subS;
-    			} else {
-    				addNewCompactNode(currentNode, subSSuffix);
-    			}
-    		}
-    		return;
-    	}
-    	int maxLCP = Math.min(subS.length(), currentNode.string.length());
-    	for (int i = 0; i < maxLCP; i++) {
-    		if (!subS.substring(i, i + 1).equals(currentNode.string.substring(i, i + 1))) {
-    			createBranch(subS, currentNode, i);
-    			return;
-    		}
-    	}
-    }
+//    static void insertPrefix(TrieNode currentNode, String subS) {
+//    	// Check of subS is a prefix of the current node's prefix, is so no action needed, return
+//    	if (subS.length() < currentNode.string.length() && subS.equals(currentNode.string.substring(0, subS.length()))) return;
+//    	// Check of current node's prefix is a prefix of subS, 
+//    	if (subS.length() > currentNode.string.length() && currentNode.string.equals(subS.substring(0, currentNode.string.length()))) {
+//    		char nextChar = subS.charAt(currentNode.string.length());
+//    		int index = nextChar - 'a';
+//    		String subSSuffix = subS.substring(currentNode.string.length(), subS.length());
+//    		if (currentNode.letterArray[index] != null) {
+//    			insertPrefix(currentNode.letterArray[index], subSSuffix);
+//    		} else {
+//    			boolean letterArrayIsEmpty = true;
+//    			for (int i = 0; i < currentNode.letterArray.length; i++) {
+//    				if (currentNode.letterArray[i] != null) {
+//    					letterArrayIsEmpty = false;
+//    					break;
+//    				}
+//    			}
+//    			if (letterArrayIsEmpty) {
+//    				currentNode.string = subS;
+//    			} else {
+//    				addNewCompactNode(currentNode, subSSuffix);
+//    			}
+//    		}
+//    		return;
+//    	}
+//    	int maxLCP = Math.min(subS.length(), currentNode.string.length());
+//    	for (int i = 0; i < maxLCP; i++) {
+//    		if (!subS.substring(i, i + 1).equals(currentNode.string.substring(i, i + 1))) {
+//    			createBranch(subS, currentNode, i);
+//    			return;
+//    		}
+//    	}
+//    }
 
-	static void createBranch(String subS, TrieNode oldNode, int lcp) {
-		TrieNode replacementNode = addNewCompactNode(oldNode.parentNode, oldNode.string.substring(0, lcp));
-		addNewCompactNode(replacementNode, subS.substring(lcp, subS.length()));
-		oldNode.string = oldNode.string.substring(lcp, oldNode.string.length());
-		char c = oldNode.string.charAt(0);
+	static void createBranch(int start, int end, TrieNode oldNode, int lcp) {
+		TrieNode replacementNode = addNewCompactNode(oldNode.parentNode, oldNode.stringStart, oldNode.stringStart + lcp - 1);
+		addNewCompactNode(replacementNode, start + lcp, end);
+//		oldNode.string = oldNode.string.substring(lcp, oldNode.string.length());
+		char c = s.charAt(oldNode.stringStart);
 		int oNodeIndex = c - 'a';
 		replacementNode.letterArray[oNodeIndex] = oldNode;
 		oldNode.parentNode = replacementNode;
 	}
 	
-	static TrieNode addNewCompactNode(TrieNode parentNode, String string) {
-		TrieNode newNode = new TrieNode();
+	static TrieNode addNewCompactNode(TrieNode parentNode, int start, int end) {
+		TrieNode newNode = new TrieNode(start, end);
 		if (parentNode == null) {
 			trieTreeRoot = newNode;
 		} else {
 			newNode.parentNode = parentNode;
-			char c = string.charAt(0);
+			char c = s.charAt(start);
 			int index = c - 'a';
 			parentNode.letterArray[index] = newNode;
 		}
-		newNode.string = string;
+//		newNode.string = string;
 		return newNode;
 		
 	}
     
-    static int countNodesInTrie(TrieNode node) {
-    	int stringCount = node.string.length();
-
-    	for (int i = 0; i < 26; i++) {
-    		if (node.letterArray[i] != null) {
-    			stringCount += countNodesInTrie(node.letterArray[i]);
-    		}
-    	}
-    	return stringCount;
-    }
+//    static int countNodesInTrie(TrieNode node) {
+//    	int stringCount = node.string.length();
+//
+//    	for (int i = 0; i < 26; i++) {
+//    		if (node.letterArray[i] != null) {
+//    			stringCount += countNodesInTrie(node.letterArray[i]);
+//    		}
+//    	}
+//    	return stringCount;
+//    }
     
 }
 
--- how_many_substrings/test/how_many_substrings/TestSolution.java
+++ how_many_substrings/test/how_many_substrings/TestSolution.java
@@ -8,11 +8,11 @@ public class TestSolution {
 	
 	@Test
 	public void testAddNewCompactNode() {
-		String string = "abc";
-		TrieNode newNode = Solution.addNewCompactNode(null, string);
+		Solution.s = "abc";
+		TrieNode newNode = Solution.addNewCompactNode(null, 0, 3);
 		assertEquals(newNode, Solution.trieTreeRoot);
 		assertEquals(null, newNode.parentNode);
-		assertEquals(string, newNode.string);
+		assertEquals(Solution.s, Solution.s.substring(newNode.stringStart, newNode.stringEnd));
 		boolean letterArrayIsEmpty = true;
 		for (int i = 0; i < newNode.letterArray.length; i++) {
 			if (newNode.letterArray[i] != null) {
@@ -22,91 +22,94 @@ public class TestSolution {
 		}
 		assertEquals(true, letterArrayIsEmpty);
 
-		TrieNode nodeD = Solution.addNewCompactNode(newNode, "d");
-		TrieNode nodeX = Solution.addNewCompactNode(newNode, "x");
+		Solution.s = "abcdabcx";
+		TrieNode nodeD = Solution.addNewCompactNode(newNode, 3, 4);
+		TrieNode nodeX = Solution.addNewCompactNode(newNode, 7, 8);
 		assertEquals(newNode, nodeD.parentNode);
 		assertEquals(newNode, nodeX.parentNode);
 		assertEquals(nodeD, newNode.letterArray[3]);
 		assertEquals(nodeX, newNode.letterArray[23]);
-		assertEquals("d", nodeD.string);
-		assertEquals("x", nodeX.string);
-		
-		
+		assertEquals("d", Solution.s.substring(nodeD.stringStart, nodeD.stringEnd));
+		assertEquals("x", Solution.s.substring(nodeX.stringStart, nodeX.stringEnd));
 	}
 
 	@Test
 	public void testCreateBranch() {
-		TrieNode startingNode = Solution.addNewCompactNode(null, "abcd");
-		String subS = "abce";
-		Solution.createBranch(subS, startingNode, 3);
+		Solution.s = "abcdabce";
+		TrieNode startingNode = Solution.addNewCompactNode(null, 0, 3);
+//		String subS = "abce";
+		int subSStart = 4;
+		int subSEnd = 7;
+		int subSLength = 7 - 4 + 1;
+		Solution.createBranch(subSStart, subSEnd, startingNode, subSLength);
 		
-		TrieNode replacementNode = Solution.trieTreeRoot;
-		assertEquals("abc", replacementNode.string);
-		
-		int index = 'd' - 'a';
-		TrieNode oldNode = replacementNode.letterArray[index];
-		assertEquals(startingNode, oldNode);
-		assertEquals("d", oldNode.string);
-		assertEquals(oldNode.parentNode, replacementNode);
-		
-		index = 'e' - 'a';
-		TrieNode newNode = replacementNode.letterArray[index];
-		assertEquals("e", newNode.string);
-		assertEquals(newNode.parentNode, replacementNode);
-		
-		startingNode = Solution.trieTreeRoot;
-		Solution.createBranch("abx", startingNode, 2);
-		
-		replacementNode = Solution.trieTreeRoot;
-		assertEquals("ab", replacementNode.string);
-		
-		index = 'c' - 'a';
-		oldNode = replacementNode.letterArray[index];
-		assertEquals(startingNode, oldNode);
-		assertEquals("c", oldNode.string);
-		assertEquals(oldNode.parentNode, replacementNode);
-		
-		index = 'x' - 'a';
-		newNode = replacementNode.letterArray[index];
-		assertEquals("x", newNode.string);
-		assertEquals(newNode.parentNode, replacementNode);
+//		TrieNode replacementNode = Solution.trieTreeRoot;
+//		assertEquals("abc", replacementNode.string);
+//		
+//		int index = 'd' - 'a';
+//		TrieNode oldNode = replacementNode.letterArray[index];
+//		assertEquals(startingNode, oldNode);
+//		assertEquals("d", oldNode.string);
+//		assertEquals(oldNode.parentNode, replacementNode);
+//		
+//		index = 'e' - 'a';
+//		TrieNode newNode = replacementNode.letterArray[index];
+//		assertEquals("e", newNode.string);
+//		assertEquals(newNode.parentNode, replacementNode);
+//		
+//		startingNode = Solution.trieTreeRoot;
+//		Solution.createBranch("abx", startingNode, 2);
+//		
+//		replacementNode = Solution.trieTreeRoot;
+//		assertEquals("ab", replacementNode.string);
+//		
+//		index = 'c' - 'a';
+//		oldNode = replacementNode.letterArray[index];
+//		assertEquals(startingNode, oldNode);
+//		assertEquals("c", oldNode.string);
+//		assertEquals(oldNode.parentNode, replacementNode);
+//		
+//		index = 'x' - 'a';
+//		newNode = replacementNode.letterArray[index];
+//		assertEquals("x", newNode.string);
+//		assertEquals(newNode.parentNode, replacementNode);
 	}
-	
-	@Test
-	public void testInsertCompactNode() {
-		Solution.trieTreeRoot = new TrieNode();
-		String subS = "abcd";
-		for (int i = 0; i < 4; i++) {
-			Solution.insertPrefix(Solution.trieTreeRoot, subS.substring(i,subS.length()));
-		}
-		
-		assertEquals("", Solution.trieTreeRoot.string);
-		TrieNode[] childNodes = new TrieNode[4];
-		for (int i = 0; i < 4; i++) {
-			childNodes[i] = Solution.trieTreeRoot.letterArray[i];
-			assertEquals(subS.subSequence(i, subS.length()), childNodes[i].string);
-			assertEquals(Solution.trieTreeRoot, childNodes[i].parentNode);
-		}
-		
-		Solution.trieTreeRoot = new TrieNode();
-		subS = "abcabc";
-		for (int i = 0; i < 6; i++) {
-			Solution.insertPrefix(Solution.trieTreeRoot, subS.substring(i,subS.length()));
-		}
-		
-		assertEquals("", Solution.trieTreeRoot.string);
-		childNodes = new TrieNode[6];
-		for (int i = 0; i < 6; i++) {
-			if (i > 2) {
-				assertEquals(null,Solution.trieTreeRoot.letterArray[i]);
-			} else {
-				childNodes[i] = Solution.trieTreeRoot.letterArray[i];
-				assertEquals(subS.subSequence(i, subS.length()), childNodes[i].string);
-				assertEquals(Solution.trieTreeRoot, childNodes[i].parentNode);
-			}
-		}
-		
-		
-	}
+//	
+//	@Test
+//	public void testInsertCompactNode() {
+//		Solution.trieTreeRoot = new TrieNode();
+//		String subS = "abcd";
+//		for (int i = 0; i < 4; i++) {
+//			Solution.insertPrefix(Solution.trieTreeRoot, subS.substring(i,subS.length()));
+//		}
+//		
+//		assertEquals("", Solution.trieTreeRoot.string);
+//		TrieNode[] childNodes = new TrieNode[4];
+//		for (int i = 0; i < 4; i++) {
+//			childNodes[i] = Solution.trieTreeRoot.letterArray[i];
+//			assertEquals(subS.subSequence(i, subS.length()), childNodes[i].string);
+//			assertEquals(Solution.trieTreeRoot, childNodes[i].parentNode);
+//		}
+//		
+//		Solution.trieTreeRoot = new TrieNode();
+//		subS = "abcabc";
+//		for (int i = 0; i < 6; i++) {
+//			Solution.insertPrefix(Solution.trieTreeRoot, subS.substring(i,subS.length()));
+//		}
+//		
+//		assertEquals("", Solution.trieTreeRoot.string);
+//		childNodes = new TrieNode[6];
+//		for (int i = 0; i < 6; i++) {
+//			if (i > 2) {
+//				assertEquals(null,Solution.trieTreeRoot.letterArray[i]);
+//			} else {
+//				childNodes[i] = Solution.trieTreeRoot.letterArray[i];
+//				assertEquals(subS.subSequence(i, subS.length()), childNodes[i].string);
+//				assertEquals(Solution.trieTreeRoot, childNodes[i].parentNode);
+//			}
+//		}
+//		
+//		
+//	}
 
 }
